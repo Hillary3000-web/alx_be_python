917492ffe918ce8b9daff4ac3bb88dc24c984edf
Hillary Chukwuma Prince <princehillary813@gmail.com>
2025-11-06 15:48:52 +0100

    Add simple interest calculation script

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+principal = 1000   # initial amount
+rate = 0.05        # 5% interest rate
+time = 3       	   # 3 years
+
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
